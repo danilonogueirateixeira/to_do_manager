1aa4acedbf3e9a50d4b6f0c20012c544c2b9997f
danilonogueirateixeira <danilo_dnt@outlook.com>
2019-05-08 17:32:26 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+import Login from './src/screens/Login';
+
+AppRegistry.registerComponent(appName, () => Login);
