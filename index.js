e54f83e985a1c84fb4f73d90dcacb845c3596f43
DANILO NOGUEIRA TEIXEIRA <43114920+danilonogueirateixeira@users.noreply.github.com>
2019-05-09 16:47:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import Login from './src/screens/Login';
+import Login from './src/screens/Login'; 
+
 
 AppRegistry.registerComponent(appName, () => Login);
