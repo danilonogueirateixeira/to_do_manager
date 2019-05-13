fd4256f72bb01abaaa3025f9222df12291439e53
danilonogueirateixeira <danilo_dnt@outlook.com>
2019-05-13 10:00:34 -0300

    inclusão de rotas para ToDoTasks e DoneTasks

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,10 +1,29 @@
-import { createStackNavigator, creatAppContainer, createAppContainer } from 'react-navigation';
-import { Login, Register } from '../screens/Screens';
+import { createStackNavigator, createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import { Login, Register, ToDoTasks, DoneTasks } from '../screens/Screens';
+import { Platform } from 'react-native';
+
+const taskListTabNavigator = createBottomTabNavigator({
+    pageToDoTasks: { screen: ToDoTasks, title: 'To Do'},
+    pageDoneTasks: { screen: DoneTasks, title: 'Done'}
+});
 
 export default Routes = createAppContainer(createStackNavigator(
     {
         pageLogin: { screen: Login },
-        pageRegister: { screen: Register }
+        pageRegister: { screen: Register },
+        pageTasksList: {
+            screen: taskListTabNavigator,
+            navigatoionOptions: {
+                ...Platform.select({
+                    ios: {
+                        title: 'Task List'
+                    },
+                    android: {
+                        header: null
+                    }
+                })
+            }
+        }
     }, {
         headerMode: 'screen'
     }
