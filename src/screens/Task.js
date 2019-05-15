74fcdc0f52834472b05efe2ec5d52df6b3cc8efb
danilonogueirateixeira <danilo_dnt@outlook.com>
2019-05-15 16:32:44 -0300

    Inclusao de classe Task, que adiciona uma nova tarefa

---
+++ src/screens/Task.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import { View, TextInput, Switch, Text, Button, StyleSheet, Alert } from 'react-native';
+import { writeTaskOnFirebase, writeTaskOnFirebaseAsync } from '../services/FirebaseApi';
+
+export default class Task extends Component {
+    static navigationOptions = {
+        title: 'Task'
+    }
+
+    state = {
+        title: '',
+        resume: '',
+        priority: true,
+        isDone: false
+    };
+
+    render() {
+        return (
+            <View style={styles.conteiner}>
+                <TextInput style={styles.input}
+                    placeholder='Title'
+                    value={this.state.title}
+                    onChangeText={(value) => this.setState({ title: value })} />
+                <TextInput style={[styles.input, styles.multilineInput]}
+                    placeholder='Resume'
+                    multiline={true} numberOfLines={4}
+                    value={this.state.resume}
+                    onChangeText={(value => this.setState({resume: value}))} />
+                <View style={styles.switchContainer}>
+                    <Switch value={this.state.priority}
+                        onValueChange={(value) => this.setState({ priority: value})}
+                        value={this.state.priority} />
+                    <Text style={styles.switchText}>Hight Priority</Text>
+                </View>
+                <View style={styles.switchContainer}>
+                    <Switch value={this.state.isDone}
+                        onValueChange={(value) => this.setState({ isDone: value})}
+                        value={this.state.isDone} />
+                    <Text style={styles.switchText}>Is Done?</Text>
+                </View>
+                <Button style={styles.button}
+                    title='Save'
+                    onPress={async () => this._saveTaskAsync()} />
+            </View>
+        );
+    }
+
+    async _saveTaskAsync() {
+        var task = {
+            title: this.state.title,
+            resume: this.state.resume,
+            priority: this.state.priority,
+            isDone: this.state.isDone
+        };
+
+        try {
+            await writeTaskOnFirebaseAsync(task);
+            this.props.navigation.goBack();
+        } catch (error) {
+            Alert.alert("Erro Saving", error.message);
+        }
+    }
+
+}
+
+
+
+
+const styles = StyleSheet.create({
+    conteiner: {
+        flex: 1,
+        padding: 20
+    },
+    input: {
+        marginBottom: 20
+    },
+    multilineInput: {
+        height: 100
+    },
+    switchContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingBottom: 20
+    },
+    switchText: {
+        marginLeft: 10,
+        color: 'black',
+        fontSize: 18
+    }
+});
