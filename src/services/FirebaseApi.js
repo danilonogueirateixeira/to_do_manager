c4dbdfd19682145d224c8e4c673f32cdcdc45f44
Danilo <danilo_dnt@outlook.com>
2019-05-17 21:04:37 -0300

    Edicao do metodo writeTaskOnFirebaseAsync verificando a chave para edicao

--- src/services/FirebaseApi.js
+++ src/services/FirebaseApi.js
@@ -48,10 +48,12 @@ export const writeTaskOnFirebaseAsync = async (task) => {
         .database()
         .ref(user.uid);
 
-    const key = tasksReference
-        .child('tasks')
-        .push()
-        .key;
+    const key = task.key ?
+        task.key :
+        tasksReference
+            .child('tasks')
+            .push()
+            .key;
         
     return await tasksReference
         .child(`tasks/${key}`)
